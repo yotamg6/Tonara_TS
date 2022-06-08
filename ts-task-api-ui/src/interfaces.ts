d76dcd25b8aaeecd50fbe4f10193d073b36805a9
yotamg6 <93833968+yotamg6@users.noreply.github.com>
2022-06-08 13:30:47 +0300

    work on updates

--- ts-task-api-ui/src/interfaces.ts
+++ ts-task-api-ui/src/interfaces.ts
@@ -3,6 +3,6 @@ export interface AssignmentInter {
   description?: string;
   music_genre?: string;
   practice_time?: string | number;
-  days?: number | number;
-  days_practiced?: number | number;
+  days?: number;
+  days_practiced?: number;
 }
